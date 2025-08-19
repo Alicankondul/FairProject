d3ea9f1fcab0aa9345e7ba364e78ca29b3ee5dbc
alicankondul <alicankondul@gmail.com>
2025-08-19 11:20:31 +0300

    WinFormUI katmanındaki eksiklikler giderildi.

--- Project.WinFormUI/Program.cs
+++ Project.WinFormUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project.WinFormUI.Forms.LoginForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Project.WinFormUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
